d4b72037b4b1add4f0e784fbd7658a52c9202a49
SebSeitz <sebastian_seitz13@web.de>
2021-12-02 21:21:15 +0100

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
