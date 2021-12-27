fa6199e205a2668a4ef6d846998bbd647cdb55e6
SebSeitz <sebastian_seitz13@web.de>
2021-12-27 11:23:05 +0100

    form updated, grey borders

--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -4,12 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-contactform',
   templateUrl: './contactform.component.html',
   styleUrls: ['./contactform.component.scss'],
-  styles: [`input.ng-invalid{border-left: 5px solid red;}
+  styles: [`input.ng-invalid{border-left: 5px solid grey;}
   input.ng-valid{border-left: 5px solid green;}`]
 })
 export class ContactformComponent implements OnInit {
-  placeholder = 'Angular';
-  firstname = "";
 
   onSubmit(value: any) {
     console.log(value);
