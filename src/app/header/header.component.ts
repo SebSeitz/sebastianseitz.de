4de810ef2c48279b15dcf4a0e1e8a07cd4873fe5
SebSeitz <sebastian_seitz13@web.de>
2022-01-15 00:36:30 +0100

    added profile pic rotation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,28 +7,30 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  scroll(selector: string) {
-    const element = document.querySelector(selector);
-    element ? element.scrollIntoView({ behavior: "smooth" }) : null;
-    this.menuIsOpen=false;
+
+scroll(selector: string) {
+  const element = document.querySelector(selector);
+  element ? element.scrollIntoView({ behavior: "smooth" }) : null;
+  this.menuIsOpen = false;
+}
+
+menuIsOpen = false;
+openCloseMenu() {
+  if (!this.menuIsOpen) {
+    this.menuIsOpen = true;
+  }
+  else {
+    this.menuIsOpen = false;
   }
 
-  menuIsOpen = false;
- openCloseMenu() {
-    if (!this.menuIsOpen) {
-      this.menuIsOpen=true;
-    }
-    else {
-      this.menuIsOpen=false;
-    }
-
-  }
+}
 
 
-  @Input() alternativeMode = true;
-  constructor() { }
+@Input() alternativeMode = true;
+constructor() { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+
+}
 
 }
