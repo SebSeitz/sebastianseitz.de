16ca5a68074ba83bb5ad59a7e3ee8012619a12ea
SebSeitz <sebastian_seitz13@web.de>
2022-01-15 22:26:08 +0100

    added explanations to skills

--- src/app/myskills/myskills.component.ts
+++ src/app/myskills/myskills.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./myskills.component.scss']
 })
 export class MyskillsComponent implements OnInit {
-
+  javascriptTip = false;
+  angularTip = false;
+  HTMLtip = false;
+  scrumTip = false;
+  gitTip = false;
+  designTip = false;
+  restAPI = false;
+  databasesTip = false;
+  testTip = false;
   constructor() { }
 
   ngOnInit(): void {
